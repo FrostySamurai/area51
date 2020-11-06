a4b82afccb1b61c7666b5ff2668cfcfb9660583c
Martin Hajdin <epator85@gmail.com>
2020-11-07 00:20:11 +0100

    PlayerController update

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,45 +3,62 @@
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class PlayerController : MonoBehaviour
 {
+    public enum PlayerState
+    {
+        None,
+        Crouch,
+        Hidden,
+        Climbing
+    }
+
     [SerializeField, Range(0, 0.5f)] private float _movementSmoothStep = .05f;
     [SerializeField] private float _playerSpeed = 5f;
+    [SerializeField] private float _playerCrouchSpeed = 5f;
     [SerializeField] private float _playerClimbingSpeed = 5f;
     [SerializeField] private float _jumpForce = 500f;
     [SerializeField] private float offset = 0;
     [SerializeField] private Transform _groundCheckPosition;
     [SerializeField] private float _groundCheckRadius;
     [SerializeField] private LayerMask _groundLayer;
-    
-    private Vector2 _velocity;
 
+    private PlayerState _playerState;  
+    private Vector2 _velocity;
 
     // components
     private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
     
     // flags
     private bool _isGrounded = true;
-    
+    private bool _jumpFromLadge = true;
+
     // controls
     private float _horizontalMovement = 0;
     private float _verticalMovement = 0;
     private bool _jump = false;
     private bool _isClimbing = false;
 
+    //input controller
+    private InputController _inputController;
+    public InputController InputController 
+    {
+        get 
+        {
+            if (_inputController == null)
+                _inputController = FindObjectOfType<InputController>();
+            return _inputController;
+        } 
+    }
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _playerState = PlayerState.None;
     }
 
     public void Update()
     {
-        _horizontalMovement = Input.GetAxis("Horizontal");
-        _verticalMovement = Input.GetAxis("Vertical");
-
-        if (_isGrounded && Input.GetButtonDown("Jump"))
-        {
-            _jump = true;          
-        }
-
         if (!_isGrounded && CheckGround())
         {
             _isGrounded = true;
@@ -53,37 +70,74 @@ public class PlayerController : MonoBehaviour
         Movement();
     }
 
-
-
     void Movement() 
     {
-        Vector2 targetVelocity = Vector2.zero;
-        if (_isClimbing) 
+        Vector2 move = Vector2.zero;
+        switch (_playerState)
         {
-            targetVelocity = new Vector2(_horizontalMovement * _playerSpeed, _verticalMovement * _playerClimbingSpeed);
-            _rb.gravityScale = 0;
-        } 
-        else 
-        {
-            targetVelocity = new Vector2(_horizontalMovement * _playerSpeed, _rb.velocity.y);
-            _rb.gravityScale = 1;
+            case PlayerState.None:
+                move = HorizontalMovement();
+                Jump();               
+                break;
+            case PlayerState.Crouch:
+                CrouchMovement();
+                break;
+            case PlayerState.Climbing:
+                move = VerticalMovement();
+                break;
         }
 
-        if (_jump && _isGrounded)
-        {
-            _isGrounded = false;
-            _jump = false;
-            _rb.AddForce(new Vector2(0f, _jumpForce));
-        }
+        _rb.velocity = Vector2.SmoothDamp(_rb.velocity, move, ref _velocity, _movementSmoothStep);
+    }
 
-        _rb.velocity = Vector2.SmoothDamp(_rb.velocity, targetVelocity, ref _velocity, _movementSmoothStep);
+    Vector2 HorizontalMovement() 
+    {
+        float horizontalMovement = InputController.HorizontalMovement * _playerSpeed;
+        Vector2 velocity = new Vector2(horizontalMovement, _rb.velocity.y);
+        return velocity;
+    }
+
+    Vector2 VerticalMovement() 
+    {
+        float verticalMovement = InputController.VerticalMovement * _playerClimbingSpeed;
+        float horizontalMovement = InputController.HorizontalMovement * _playerSpeed;
+        Vector2 velocity = new Vector2(horizontalMovement, verticalMovement);
+        return velocity;
+    }
+
+    void CrouchMovement() 
+    {
         
     }
 
+    void Jump() 
+    {
+        if (InputController.Jump && _isGrounded)
+        {
+            _isGrounded = false;
+            InputController.Jump = false;
+            _rb.AddForce(new Vector2(0f, _jumpForce));
+        }
+    }
 
     Collider2D CheckGround() 
     {
         Collider2D ground = Physics2D.OverlapCircle(_groundCheckPosition.position, _groundCheckRadius, _groundLayer);
         return ground;
-    }      
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder")) 
+        {
+            _playerState = PlayerState.Climbing;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _playerState = PlayerState.None;
+    }
+
 }
