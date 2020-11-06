fc8047251adfa24c2884ad2b5ac6859b94fb3835
Michal <hudek.michal@gmail.com>
2020-11-06 21:18:55 +0100

    Enemy controller.

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed = 1f;
+    [SerializeField] private float _patrolPointStayTime = 1f;
+    [SerializeField] private float _patrolPointProximity = 0.3f;
+
+    [SerializeField] private Transform _patrolPointsParent = null;
+    
+    private float _timeToMove = 0f;
+
+    private Queue<Transform> _patrolPoints = new Queue<Transform>();
+    private Transform _targetPatrolPoint = null;
+
+    private Rigidbody2D _rigidbody = null;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        PopulatePatrolPoints();
+        GoToNextPatrolPoint();
+    }
+
+    private void Update()
+    {
+        WaitAtPatrolPoint();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void WaitAtPatrolPoint()
+    {
+        if (_targetPatrolPoint == null || _timeToMove <= 0f)
+            return;
+
+        _timeToMove -= Time.deltaTime;
+
+        if (_timeToMove > 0f)
+            return;
+
+        GoToNextPatrolPoint();
+    }
+
+    private void Move()
+    {
+        if (_targetPatrolPoint == null || _timeToMove > 0f)
+            return;
+
+        float targetX = _targetPatrolPoint.position.x;
+        float enemyX = transform.position.x;
+
+        if (Mathf.Abs(targetX - enemyX) <= _patrolPointProximity)
+        {
+            ReachedPatrolPoint();
+            return;
+        }
+
+        float direction = targetX < enemyX ? -1f : 1f;
+
+        transform.localScale = new Vector2(direction, 1f);
+        _rigidbody.velocity = new Vector2(_movementSpeed * direction, 0f);
+    }
+
+    private void ReachedPatrolPoint()
+    {
+        if (_targetPatrolPoint == null)
+            return;
+
+        _rigidbody.velocity = Vector2.zero;
+
+        if (_patrolPointStayTime > 0f)
+        {
+            _timeToMove = _patrolPointStayTime;
+            return;
+        }
+
+        GoToNextPatrolPoint();
+    }
+
+    private void PopulatePatrolPoints()
+    {
+        if (_patrolPoints == null)
+            return;
+
+        for (int i = 0; i < _patrolPointsParent.childCount; i++)
+            _patrolPoints.Enqueue(_patrolPointsParent.GetChild(i));
+    }
+
+    private void GoToNextPatrolPoint()
+    {
+        if (_targetPatrolPoint != null)
+            _patrolPoints.Enqueue(_targetPatrolPoint);
+
+        if (_patrolPoints.Count == 0)
+            return;
+
+        _targetPatrolPoint = _patrolPoints.Dequeue();
+    }
+}
+
