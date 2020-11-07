7ac423dc0eb3d77b08cfb60e08d18b6366581c67
Michal <hudek.michal@gmail.com>
2020-11-07 10:32:51 +0100

    Added conveyor.

---
+++ Assets/Scripts/Platforms/Conveyor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    private SurfaceEffector2D _surfaceEffector = null;
+
+    private void Awake()
+    {
+        if (!TryGetComponent(out _surfaceEffector))
+            Debug.LogError($"{name} is missing SurfaceEffector2D.");
+    }
+
+    public void ToggleEnabled(bool enabled)
+    {
+        if (_surfaceEffector != null)
+            _surfaceEffector.enabled = enabled;
+
+        // MH: handle animations
+    }
+}
+
