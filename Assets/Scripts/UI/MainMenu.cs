9decb4384ea70976eb6ae092e8ff09cd62bf5c64
Martin Hajdin <epator85@gmail.com>
2020-11-08 17:34:22 +0100

    Buttons in menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,13 +4,8 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private RectTransform _leftPointer;
-    [SerializeField] private RectTransform _rightPointer;
     [SerializeField] private List<RectTransform> _menuItems;
 
-    [SerializeField] private float _arrowOffset;
-    [SerializeField] private float _animationPeriod;
-    [SerializeField] private float _animationSpeed;
 
     private RectTransform _selectedItem = null;
     private int _selectedItemIndex = 0;
@@ -40,24 +35,21 @@ public class MainMenu : MonoBehaviour
 
     private void Update()
     {
-        if (AppData.InputController.VerticalMovement == 0) _releaseButton = true;
-        if (_releaseButton && AppData.InputController.VerticalMovement < 0) { _selectedItemIndex++; _releaseButton = false; _reDrawArrows = true; }
-        if (_releaseButton && AppData.InputController.VerticalMovement > 0) { _selectedItemIndex--; _releaseButton = false; _reDrawArrows = true; }
-        
+        if (AppData.InputController.HorizontalMovement == 0) _releaseButton = true;
+        if (_releaseButton && AppData.InputController.HorizontalMovement < 0) { _selectedItemIndex++; _releaseButton = false; _reDrawArrows = true; }
+        if (_releaseButton && AppData.InputController.HorizontalMovement > 0) { _selectedItemIndex--; _releaseButton = false; _reDrawArrows = true; }
+
         _selectedItemIndex = _selectedItemIndex < 0 ? _menuItems.Count - 1 : _selectedItemIndex > _menuItems.Count - 1 ? 0 : _selectedItemIndex;
-
-        if (_reDrawArrows) SetArrowPositon();
-        SimpleAnim(_leftPointer);
-        SimpleAnim(_rightPointer, -1);
-    }
-
-    void SetArrowPositon() 
-    {
         _selectedItem = _menuItems[_selectedItemIndex];
+        ScaleAnimation(_selectedItem.GetComponent<RectTransform>());
 
-        _leftPointer.transform.position = _selectedItem.transform.position - Vector3.right * _arrowOffset;
-        _rightPointer.transform.position = _selectedItem.transform.position + Vector3.right * _arrowOffset;
-        _reDrawArrows = false;
+        for (int i= 0; i< _menuItems.Count; i++)
+        {
+            if (_selectedItemIndex != i) 
+            {
+                _menuItems[i].sizeDelta = new Vector2(150, 150);
+            }
+        }
     }
 
     private void Select()
@@ -65,11 +57,11 @@ public class MainMenu : MonoBehaviour
         _selectedItem.GetComponent<Button>().onClick.Invoke();
     }
 
-    void SimpleAnim(RectTransform transform, int orientation = 1) 
+    void ScaleAnimation(RectTransform rect) 
     {
-        float value = Mathf.Sin(Time.time * _animationPeriod) * orientation;
-        Vector2 anim = new Vector2(transform.position.x + value * _animationSpeed, transform.position.y);
-        transform.position = anim;
+        float value = Mathf.Sin(Time.time * 10f);
+        Vector2 anim = new Vector2(rect.sizeDelta.x + value, rect.sizeDelta.y + value);
+        rect.sizeDelta = anim;
     }
 }
 
