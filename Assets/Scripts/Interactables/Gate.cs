571d3ce3745961c5ed8c041f1b9799b57c6073bd
Michal <hudek.michal@gmail.com>
2020-11-08 12:40:16 +0100

    Changed the way gate opens. Added elevator image and anims.

--- Assets/Scripts/Interactables/Gate.cs
+++ Assets/Scripts/Interactables/Gate.cs
@@ -6,12 +6,35 @@ public class Gate : MonoBehaviour
 {
     [SerializeField] private SceneName scene;
 
+    private bool _isOpen = false;
+    private Animator _animator = null;
+    private AudioSource _audioSource = null;
+
+    private void Awake()
+    {
+        if (!TryGetComponent(out _animator))
+            Debug.LogError($"{name} is missing Animator.");
+
+        if (!TryGetComponent(out _audioSource))
+            Debug.LogError($"{name} is missing AudioSource.");
+    }
+
+    public void Open()
+    {
+        if (_isOpen)
+            return;
+
+        _isOpen = true;
+        _animator.SetTrigger("Open");
+        _audioSource.Play();
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player")) 
         {
             Debug.Log(collision.gameObject.layer);
-            if (AppData.GameManager.LevelIsDone && AppData.InputController.VerticalMovement > 0)
+            if (_isOpen && AppData.InputController.VerticalMovement > 0)
             {
                 AppData.SceneLoader.LoadScene(scene);
             }
